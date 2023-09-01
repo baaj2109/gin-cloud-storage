c5ca84bf44f69831e5200b37ac10a5420f3f5335
kehwa weng <kehwaweng@ipevodeMacBook-Pro-2.local>
2023-09-01 16:56:03 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/baaj2109/gin-cloud-storage/router"
@@ -48,29 +53,22 @@ func main() {
 		IdleTimeout:  10 * time.Second,
 	}
 
-	// shutdownError := make(chan error)
-	// go func() {
-	// 	quit := make(chan os.Signal, 1)
-	// 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	// 	signal := <-quit
-	// 	fmt.Println("shutting down server", map[string]string{
-	// 		"signal": signal.String(),
-	// 	})
-	// 	// app.logger.Printf("shutting down server %s", s)
-	// 	// os.Exit(0)
-	// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// 	defer cancel()
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-	// 	shutdownError <- s.Shutdown(ctx)
-	// }()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
 
-	if err := s.ListenAndServe(); err != nil {
-		panic(err)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
+	log.Println("Server exiting")
 
-	// err := <-shutdownError
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("starting %s server on 8080")
 }
